87de38646dac649d996546d3ac48d1953ed41bc5
atindersandhu <sin11288@sheridancollege.ca>
2022-03-05 22:48:04 -0500

    Initial commit

---
+++ src/ca/sheridancollege/project/Card.java
@@ -0,0 +1,29 @@
+/**
+ * SYST 17796 Project Base code.
+ * Students can modify and extend to implement their game.
+ * Add your name as an author and the date!
+ */
+package ca.sheridancollege.project;
+
+/**
+ * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
+ * game. Students wishing to add to the code should remember to add themselves as a modifier.
+ *
+ * @author Atinder Pal Singh 26 Feb 2022
+ * @author Manvir Singh Rattan 26 Feb 2022
+ * @author Simranjeet Kaur 26 Feb 2022
+ * @author Harmanjot Kaur 26 Feb 2022
+ */
+ */
+public abstract class Card {
+    //default modifier for child classes
+
+    /**
+     * Students should implement this method for their specific children classes
+     *
+     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     */
+    @Override
+    public abstract String toString();
+
+}
